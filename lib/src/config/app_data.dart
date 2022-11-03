b688d073bd1edc71a47048f59460436055f1136d
Carlo Azevedo <carlos_toni_azevedo@hotmail.com>
2022-11-02 23:33:23 -0300

    update 0.6.5

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -1,42 +1,42 @@
 import 'package:vendas/src/models/item_model.dart';
 
 ItemModel item_1 = ItemModel(
-  description: '',
+  description: 'Item 1',
   imgUrl: 'assets/itens/arroz.png',
   itemName: 'Item 1',
   price: 5.5,
   unit: 'Kg',
 );
 ItemModel item_2 = ItemModel(
-  description: '',
+  description: 'Item 2',
   imgUrl: 'assets/itens/castanha.png',
   itemName: 'Item 2',
   price: 5.5,
   unit: 'Kg',
 );
 ItemModel item_3 = ItemModel(
-  description: '',
+  description: 'Item 3',
   imgUrl: 'assets/itens/farinha.png',
   itemName: 'Item 3',
   price: 5.5,
   unit: 'Kg',
 );
 ItemModel item_4 = ItemModel(
-  description: '',
+  description: 'Item 4',
   imgUrl: 'assets/itens/feijao.png',
   itemName: 'Item 4',
   price: 5.5,
   unit: 'Kg',
 );
 ItemModel item_5 = ItemModel(
-  description: '',
+  description: 'Item 5',
   imgUrl: 'assets/itens/lentilha.png',
   itemName: 'Item 5',
   price: 5.5,
   unit: 'Kg',
 );
 ItemModel item_6 = ItemModel(
-  description: '',
+  description: 'Item 6',
   imgUrl: 'assets/itens/milho.png',
   itemName: 'Item 6',
   price: 5.5,
--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,37 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:vendas/src/config/custom_colors.dart';
 import 'package:vendas/src/models/item_model.dart';
+import 'package:vendas/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({
+  ProductScreen({
     Key? key,
     required this.item,
   }) : super(key: key);
 
   final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white.withAlpha(240),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Image.asset(item.imgUrl),
-          ),
-          Expanded(
-              child: Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(40),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    offset: const Offset(0, 2),
+          // Conteúdo contents
+          Column(
+            children: [
+              Expanded(
+                child: Hero(
+                  tag: item.imgUrl,
+                  child: Image.asset(item.imgUrl),),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(40),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
                   ),
-                ]),
-          ))
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      // Nome - Quantidade Name - Quantity
+
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              item.itemName,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 27,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 30,
+                            width: 70,
+                            color: Colors.blue,
+                          ),
+                        ],
+                      ),
+                      // Preço Price
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                          color: CustomColors.customContrastColor,
+                        ),
+                      ),
+
+                      // Descrição Description
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          child: SingleChildScrollView(
+                            child: Text(
+                              item.description,
+                              style: const TextStyle(
+                                height: 1.5,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Botão Button
+                      SizedBox(
+                        height: 40,
+                        child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                          ),
+                          onPressed: () {},
+                          label: Text(
+                            'Adicionar ao Carrinho',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          icon: const Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            left: 10,
+            top: 10,
+            child: SafeArea(
+              child: IconButton(
+                onPressed: () => Navigator.of(context).pop(),
+                icon: Icon(
+                  Icons.arrow_back,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
